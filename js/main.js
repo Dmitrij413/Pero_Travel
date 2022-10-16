a25a019b9d0421ab3c80409336046f50d1de3ee5
dmitrij <dmitrij.khramov@bk.ru>
2022-10-16 22:18:02 +0400

    modified:   css/main.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click', function(){
-    offset = offset + 256;
-    if (offset > 2068) {
+    offset = offset + 380;
+    if (offset > 1710) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
 });
 
 document.querySelector('.slider-prev').addEventListener('click', function () {
-    offset = offset - 256;
+    offset = offset - 380;
     if (offset < 0) {
-        offset = 2068;
+        offset = 1710;
     }
     sliderLine.style.left = -offset + 'px';
 });
