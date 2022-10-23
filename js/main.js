adc93a137c556be6b834b7a5f6231dd398699a1f
Dmitrij <dmitrij.khramov@bk.ru>
2022-10-23 22:44:58 +0400

    new file:   .idea/watcherTasks.xml 	modified:   css/main.css 	modified:   excursions.html 	modified:   index.html 	modified:   scss/main.scss

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let widthImg = document.querySelector('.popular-card').offsetWidth
 
 document.querySelector('.slider-next').addEventListener('click', function(){
     offset = offset + widthImg + 20;
-    if (offset > widthImg * 5 + 160) {
+    if (offset > widthImg * 7 + 140) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
@@ -13,7 +13,7 @@ document.querySelector('.slider-next').addEventListener('click', function(){
 document.querySelector('.slider-prev').addEventListener('click', function () {
     offset = offset - widthImg - 20;
     if (offset < 0) {
-        offset = widthImg * 5 + 160;
+        offset = widthImg * 7 + 140;
     }
     sliderLine.style.left = -offset + 'px';
 });
