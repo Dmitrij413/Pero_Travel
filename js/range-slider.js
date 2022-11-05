2cf32c73170b6d46ca2f11f43571106612bbb1fc
Dmitrij <dmitrij.khramov@bk.ru>
2022-11-05 19:53:28 +0400

    Merge remote-tracking branch 'origin/main'

---
+++ js/range-slider.js
@@ -0,0 +1,13 @@
+const rangeSlider = document.getElementById('range-slider');
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [1000, 10000],
+    connect: true,
+    step: 1,
+    range: {
+      'min': [1000],
+      'max': [10000]
+    }
+  });
+}
