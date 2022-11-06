d659dcab935f8be165be9237952633fe35b41ea0
Dmitrij413 <dmitrij.khramov@bk.ru>
2022-11-06 13:26:59 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	excursions.html

--- js/range-slider.js
+++ js/range-slider.js
@@ -10,4 +10,25 @@ if (rangeSlider) {
       'max': [10000]
     }
   });
+
+  const input0 = document.getElementById('input-0');
+  const input1 = document.getElementById('input-1');
+  const inputs = [input0, input1];
+
+  rangeSlider.noUiSlider.on('update', function (values, handle){
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+    rangeSlider.noUiSlider.set(arr)
+  }
+
+  inputs.forEach((el, index) => {
+    el.addEventListener('change', (e) => {
+      console.log(index)
+      setRangeSlider(index, e.currentTarget.value);
+    })
+  })
 }
